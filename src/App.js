491ade1bbead42eca2d833981c0da781dec320fe
vyshmavi <vyshnavig014@gmail.com>
2020-11-10 11:02:18 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component , Fragment } from 'react';
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { }
+    }
+    render() { 
+        return ( <Fragment>
+            <h1>Welcome to Hotstar website</h1>
+        </Fragment> );
+    }
+}
+ 
+export default App;
